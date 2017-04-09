202c570799be208fdb98ebecb4805dacf0156ea5
Yotta Liu <yotta.liu@gmail.com>
2017-04-09 14:22:55 +0800

    用Python内置的json模块简化操作

--- subdiv.py
+++ subdiv.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 # 用于把shadowsocks的配置文件gui-config.json分割成单个节点的配置文件
-fr = open('/home/yotta/Downloads/gui-config.json', 'r')
-start = False
-fname = ''
-tmp = ''
-for line in fr.readlines():
-    if '[' in line:
-        start = True
-    if start:
-        if line.strip().startswith('{'):
-            tmp = '{\n'
-        elif line.strip().startswith('}'):
-            tmp += '}\n'
-            with open('/etc/shadowsocks/' + fname, 'w') as fw:
-                fw.write(tmp)
-            tmp = ''
-        else:
-            if line.strip().startswith('"server":') or line.strip().startswith('server:'):
-                fname = line.strip().rpartition(':')[2].partition('"')[2].rpartition('"')[0] + '.json'
-            tmp += '    ' + line.lstrip()
-fr.close()
+import sys
+import os
+import json
+if __name__ == '__main__':
+    source_file = '/home/yotta/Downloads/gui-config.json'
+    target_dir = '/etc/shadowsocks/'
+    args = sys.argv
+    argc = len(args)
+    if argc >= 2:
+        source_file = args[1]
+    if argc == 3:
+        target_dir = args[2]
+    with open(source_file, 'r') as fr:
+        data = json.load(fr)
+    for enum in data['configs']:
+        with open(os.path.join(target_dir, enum['server']+'.json'), 'w') as fw:
+            json.dump(enum, fw, ensure_ascii=False, indent=4)
